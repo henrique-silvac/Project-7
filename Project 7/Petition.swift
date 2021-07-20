ab27d20f3b4ee2c338499b7364bb00b0b2486678
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-07-20 11:07:47 -0300

    Parsing JSON using the Codable protocol

---
+++ Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Henrique Silva on 20/07/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
